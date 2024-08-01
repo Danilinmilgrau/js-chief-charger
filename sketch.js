795833a4fdb6fbd35d79cfd9b3129a1bb44796c6
Danilinmilgrau <00001119187060sp@al.educacao.sp.gov.br>
2024-08-01 07:50:58 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(500, 500);
+    background ("blue")
+  }
+  
+  function draw() {
+    
+    stroke ("white");
+    fill ("yellow");
+    
+    //console.log ()
+    
+      if (mouseIsPressed) {  rect(mouseX, mouseY, 40, 30);}
+    
+    
+    
+  }
